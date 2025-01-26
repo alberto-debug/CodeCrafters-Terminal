3066be486f7d0dbbe65b865f0b51b489eb73c976
Alberto <albertojunior848423803@gmail.com>
2025-01-26 11:55:21 +0300

    [changes]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-         System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
@@ -15,9 +13,8 @@ public class Main {
             System.out.println("$ ");
 
             input = scanner.nextLine();
-        }while (!input.matches(""));
+        } while (!input.matches(""));
+
             scanner.close();
-
-
     }
 }
