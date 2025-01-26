f400c9f768cd49ccda74270d60a197ebfe1f1eb4
Alberto <albertojunior848423803@gmail.com>
2025-01-26 12:07:12 +0300

    [changes]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,11 @@ public class Main {
             System.out.print("$ ");
 
             Scanner scanner = new Scanner(System.in);
-
             String input = scanner.nextLine();
+
+            if (input.equals("exit 0")){
+                break;
+            }
 
             System.out.println(input + ": command not found");
 
