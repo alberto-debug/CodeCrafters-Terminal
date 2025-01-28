b73dcc7624afa11c63b082c337ef603bb42c7dd5
Alberto <albertojunior848423803@gmail.com>
2025-01-28 17:30:58 +0300

    [changes]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,7 +34,7 @@ public class Main {
             }
 
             if (input.startsWith("echo")) {
-                String command = input.substring(5).trim();
+                String command = input.substring(4).trim();
                 command = handleSingleQuotes(command);
                 System.out.println(command);
             } else if (input.startsWith("type")) {
