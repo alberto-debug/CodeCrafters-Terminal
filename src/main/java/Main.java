fc86c5581817f4043feab556d391ced9f41560e0
Alberto <albertojunior848423803@gmail.com>
2025-01-28 18:06:05 +0300

    [changes]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -110,7 +110,7 @@ public class Main {
     public static String handleSingleQuotes(String input) {
         if (input.startsWith("'") && input.endsWith("'")) {
 
-            input = input.substring(1, input.length() - 1);
+            input = input.substring(0, input.length() - 1);
 
         }
         return input;
